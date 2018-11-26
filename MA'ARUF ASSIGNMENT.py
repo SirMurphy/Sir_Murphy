20ff656d15888c0c8bb5ba047acdae89d5ce34d8
SirMurphy <45232486+SirMurphy@users.noreply.github.com>
2018-11-26 14:00:02 +0100

    Add files via upload

---
+++ MA'ARUF ASSIGNMENT.py
@@ -0,0 +1,39 @@
+'''ASSIGNMENT'''
+
+
+
+'''ACETOBACTER = int(input("Acetobacter scores")
+BACILLUS = int(input(" BACILLUS scores ")
+CLOSTRIDIUM = int(input("CLOSTRIDIUM scores ")              
+DENETROPHY = int(input(" DENETROPHY scores ")
+ESCHERICHIA COLI = int(input(" ESCHERICHIA COLI scores ")
+FLORIDUIM = int(input(" ESCHERICHIA COLI scores ")
+
+total = (ACETOBACTER+BACILLUS+CLOSTRIDIUM+DENETROPHY+ESCHERICHIA+COLIFLORIDIUM)
+AVERAGE =(ACETOBACTER+BACILLUS+CLOSTRIDIUM+DENETROPHY+ESCHERICHIA+COLIFLORIDIUM)/2*100'''
+                
+
+score=int(input("give  me a score"))
+
+print("grade calculator")
+if (score >=70 and score <=100):
+    print (" Your grade is A")
+
+elif (score >=60 and score <=69):
+    print ("Your grade is B")
+
+elif (score >=50 and score <=59):
+    print ("Your grade is C")
+               
+elif (score >=45 and score <=49):
+    print ("Your grade is D")
+                
+elif (score >=40 and score <=44):
+    print ("Your grade is E")
+                
+elif (score >=0 and score <=39):
+    print ("Your grade is F")
+
+else:
+    print("nothing")
+               
